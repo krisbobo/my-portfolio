476205837fc6553d4741aeda97c24a7878e37d56
krisbobo <kiyongbohbo@gmail.com>
2022-06-17 00:40:57 +0100

    Update js to perform desired function

--- js/main.js
+++ js/main.js
@@ -1,20 +1,29 @@
-const navToggle = document.querySelector('#navToggle');
-const nav = document.querySelector('nav');
-const navIcon = document.querySelectorAll('.navIcon');
+const navToggle = document.querySelector('.nav-toggle');
+const navbar = document.querySelector('.mobile-nav-hidden');
+const closeHamburger = document.querySelector('.hidden');
 const hamburger = document.querySelector('#hamburger');
+const closeMenu = document.querySelector('.mobile-nav-menu');
+const navIcon = document.querySelectorAll('.navIcon');
 
-// What we want navToggle to do
+hamburger.addEventListener('click', () => {
+  navbar.classList.add('header');
+  closeHamburger.classList.add('open');
+});
 
-navToggle.addEventListener('click', () => {
-  nav.classList.toggle('header');
-  navIcon.forEach((icon) => {
-    icon.classList.toggle('d-block');
-  });
+closeHamburger.addEventListener('click', () => {
+  navbar.classList.remove('header');
+  navToggle.classList.add('hamburger');
+  closeHamburger.classList.remove('open');
+});
+
+closeMenu.addEventListener('click', () => {
+  navbar.classList.remove('header');
+  closeHamburger.classList.remove('open');
 });
 
 window.addEventListener('resize', () => {
   if (document.clientWidth > '768px') {
-    nav.classList.remove('header');
+    navbar.classList.remove('header');
     navIcon.forEach((icon) => {
       icon.classList.add('hidden');
     });
