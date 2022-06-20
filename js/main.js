da974fdf480f79af79fab32674afa4e9e5cb4810
Kiyong Bohbo <kiyongbohbo@gmail.com>
2022-06-20 11:37:33 +0100

    Hamburger menu (#9)
    * Edit file to allow for build
    
    * Edit file to enable us pair-program
    
    * Add Nav-bar for Mobile
    
    * Add Nav-bar style and utility classes for Mobile
    
    * Add menu-interaction for Mobile
    
    * Update file with partner's profile
    
    * Fix linter errors
    
    Co-authored-by: eudondian <eudondian@gmail.com>

--- js/main.js
+++ js/main.js
@@ -4,23 +4,19 @@ const closeHamburger = document.querySelector('.hidden');
 const hamburger = document.querySelector('#hamburger');
 const closeMenu = document.querySelector('.mobile-nav-menu');
 const navIcon = document.querySelectorAll('.navIcon');
-
 hamburger.addEventListener('click', () => {
   navbar.classList.add('header');
   closeHamburger.classList.add('open');
 });
-
 closeHamburger.addEventListener('click', () => {
   navbar.classList.remove('header');
   navToggle.classList.add('hamburger');
   closeHamburger.classList.remove('open');
 });
-
 closeMenu.addEventListener('click', () => {
   navbar.classList.remove('header');
   closeHamburger.classList.remove('open');
 });
-
 window.addEventListener('resize', () => {
   if (document.clientWidth > '768px') {
     navbar.classList.remove('header');
